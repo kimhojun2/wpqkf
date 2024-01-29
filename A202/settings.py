dc7732b5539dfd53ae3ee5ec42665099e3d2b4c1
kimhojun <hojoon1105@naver.com>
2024-01-29 17:06:41 +0900

    123123

--- A202/settings.py
+++ A202/settings.py
@@ -121,7 +121,7 @@ DATABASES = {
         'NAME': 'A202DB',
         'USER': 'A202user',
         'PASSWORD': 'ekdrnwkd1!',
-        'HOST': 'mysql',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
     }
 }
@@ -161,7 +161,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+
+STATIC_URL = '/static/'
+STATIC_ROOT = '/djangoproject/static/'
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,8 @@ RUN apt-get update && apt-get install -y python3-pip && apt-get clean
 WORKDIR /djangoproject/
 ADD . /djangoproject/
 RUN python -m pip install --upgrade pip
-RUN pip3 install --no-cache-dir torch
 
 RUN pip3 install -r requirements.txt
 RUN python manage.py makemigrations
+#RUN python manage.py migrate
 RUN python manage.py collectstatic --noinput
